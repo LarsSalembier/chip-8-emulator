4ed1cf4bf2018396f51f6b2aeceda4c368dd68b4
larssalembier <lars.salembier@gmail.com>
2023-09-13 12:40:58 +0200

    refactor

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -22,37 +22,34 @@ impl Keyboard {
         }
     }
 
-    pub fn _press_key(&mut self, key: u8) -> Result<(), KeyboardError> {
+    fn _modify_key(&mut self, key: u8, state: Key) -> Result<(), KeyboardError> {
         match self.keys.get_mut(key as usize) {
             Some(k) => {
-                *k = Key::Pressed;
+                *k = state;
                 Ok(())
             }
             None => Err(KeyboardError::KeyOutOfBounds),
         }
     }
 
+    pub fn _press_key(&mut self, key: u8) -> Result<(), KeyboardError> {
+        self._modify_key(key, Key::Pressed)
+    }
+
     pub fn _release_key(&mut self, key: u8) -> Result<(), KeyboardError> {
-        match self.keys.get_mut(key as usize) {
-            Some(k) => {
-                *k = Key::Released;
-                Ok(())
-            }
-            None => Err(KeyboardError::KeyOutOfBounds),
-        }
+        self._modify_key(key, Key::Released)
     }
 
     pub fn is_key_pressed(&self, key: u8) -> Result<bool, KeyboardError> {
         match self.keys.get(key as usize) {
-            Some(&Key::Pressed) => Ok(true),
-            Some(_) => Ok(false),
+            Some(&key_state) => Ok(key_state == Key::Pressed),
             None => Err(KeyboardError::KeyOutOfBounds),
         }
     }
 
     pub fn get_pressed_key(&self) -> Option<u8> {
-        for (i, key) in self.keys.iter().enumerate() {
-            if *key == Key::Pressed {
+        for (i, &key) in self.keys.iter().enumerate() {
+            if key == Key::Pressed {
                 return Some(i as u8);
             }
         }
